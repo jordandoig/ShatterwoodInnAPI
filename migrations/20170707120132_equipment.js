ab88e804891e34999dc26b39ec278e3aaba59d12
Jordan Doig <jordandoig@live.com>
2017-07-08 11:18:23 -0600

    Added background and sheet migrations/seeds, filled in equipment migration and partial weapon

--- migrations/20170707120132_equipment.js
+++ migrations/20170707120132_equipment.js
@@ -1,7 +1,12 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('equipment', function (table) {
-
+    table.increments();
+    table.string('name');
+    table.boolean('is_weapon');
+    table.boolean('is_armor');
+    table.integer('weight');
+    table.integer('price');
   });
 };
 
