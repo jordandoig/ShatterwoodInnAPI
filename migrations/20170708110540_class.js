d7c7bc889bed63b770f9d3fa7d9504b47e6231e1
Jordan Doig <jordandoig@live.com>
2017-07-10 11:24:16 -0600

    Filled in migration for class

--- migrations/20170708110540_class.js
+++ migrations/20170708110540_class.js
@@ -1,7 +1,22 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('class', function (table) {
-
+    table.increments();
+    table.string('name');
+    table.string('hit_die');
+    table.string('weapon_profs');
+    table.string('armor_profs');
+    table.string('tool_profs');
+    table.string('primary_ability');
+    table.boolean('str_save');
+    table.boolean('dex_save');
+    table.boolean('con_save');
+    table.boolean('int_save');
+    table.boolean('wis_save');
+    table.boolean('cha_save');
+    table.string('skill_options');
+    table.string('prof_bonus');
+    table.integer('phb_page');
   })
 };
 
