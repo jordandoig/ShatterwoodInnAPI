cfc431d5a8602f30f30a57a14dea19960961efbc
Jordan Doig <jordandoig@live.com>
2017-07-10 09:21:12 -0600

    Filled in migration for spell

--- migrations/20170707120157_spell.js
+++ migrations/20170707120157_spell.js
@@ -1,7 +1,17 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('spell', function (table) {
-
+    table.increments();
+    table.string('name');
+    table.integer('level');
+    table.string('casting_time');
+    table.string('range');
+    table.string('components');
+    table.string('duration');
+    table.string('school');
+    table.string('target');
+    table.string('area_of_effect');
+    table.integer('phb_page');
   });
 };
 
