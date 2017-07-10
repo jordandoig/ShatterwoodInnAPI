9c3b9d57f32382dbf00158aee00c80538d67c7af
Jordan Doig <jordandoig@live.com>
2017-07-10 11:40:13 -0600

    Added background_equipment migration and seed, filled in background migration

--- migrations/20170708111713_background.js
+++ migrations/20170708111713_background.js
@@ -1,7 +1,24 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('background', function (table) {
-
+    table.increments();
+    table.string('name');
+    table.string('desc');
+    table.string('alt_name');
+    table.string('alt_desc');
+    table.string('skill_profs');
+    table.string('tool_profs');
+    table.string('languages');
+    table.string('suggested_personality_traits');
+    table.string('suggested_ideals');
+    table.string('suggested_bonds');
+    table.string('suggested_flaws');
+    table.string('feature_title');
+    table.string('feature_desc');
+    table.string('misc_title');
+    table.string('misc_desc');
+    table.string('misc_options');
+    table.integer('phb_page');
   })
 };
 
