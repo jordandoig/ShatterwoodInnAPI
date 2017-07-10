f707f699cf193acc1db2f615a43f98a334b5f162
Jordan Doig <jordandoig@live.com>
2017-07-10 13:50:31 -0600

    Filled in migration for tool

--- migrations/20170708120413_tool.js
+++ migrations/20170708120413_tool.js
@@ -1,7 +1,11 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('tool', function (table) {
-
+    table.increments();
+    table.integer('equipment_id').references('id').inTable('equipment');
+    table.string('name');
+    table.string('desc');
+    table.integer('phb_page');
   })
 };
 
