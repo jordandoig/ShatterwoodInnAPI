048b19308597eeed6e57e2269bf762c2f625ec87
Jordan Doig <jordandoig@live.com>
2017-07-10 09:36:03 -0600

    Filled in migration for dragonborn_color

--- migrations/20170708110416_dragonborn_color.js
+++ migrations/20170708110416_dragonborn_color.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('dragonborn_color', function (table) {
+    table.increments();
+    table.string('color');
+    table.string('damage_type');
+    table.boolean('is_line');
+    table.boolean('is_cone');
+    table.integer('phb_page');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('dragonborn_color');
 };
