d24b74dabd6546aeb044758c9a9a5035da0ab5a3
Jordan Doig <jordandoig@live.com>
2017-07-10 09:13:02 -0600

    Filled in migrations for weapon and armor

--- migrations/20170707120142_weapon.js
+++ migrations/20170707120142_weapon.js
@@ -1,7 +1,26 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('weapon', function (table) {
-
+    table.increments();
+    table.integer('equipment_id').references('id').inTable('equipment');
+    table.string('ability_mod');
+    table.string('damage_die');
+    table.string('damage_type');
+    table.string('range');
+    table.integer('magic_mod');
+    table.string('magic_desc');
+    table.boolean('is_ammunition');
+    table.boolean('is_finesse');
+    table.boolean('is_heavy');
+    table.boolean('is_light');
+    table.boolean('is_loading');
+    table.boolean('is_range');
+    table.boolean('is_reach');
+    table.boolean('is_special');
+    table.boolean('is_thrown');
+    table.boolean('is_twohanded');
+    table.boolean('is_versatile');
+    table.integer('phb_page');
   });
 };
 
--- migrations/20170707120147_armor.js
+++ migrations/20170707120147_armor.js
@@ -1,7 +1,19 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('armor', function (table) {
-
+    table.increments();
+    table.integer('equipment_id').references('id').inTable('equipment');
+    table.integer('ac_bonus');
+    table.integer('magic_mod');
+    table.string('magic_desc');
+    table.boolean('is_light');
+    table.boolean('is_medium');
+    table.boolean('is_heavy');
+    table.boolean('is_shield');
+    table.boolean('add_dex');
+    table.integer('str_req');
+    table.boolean('stealth_disadvantage');
+    table.integer('phb_page');
   });
 };
 
