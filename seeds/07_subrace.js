dc7b6fa07542d56d0bf05b487a6702f72ef2b9a5
Jordan Doig <jordandoig@live.com>
2017-07-13 13:51:16 -0600

    Rolled back and remigrated, seeded subrace

--- seeds/07_subrace.js
+++ seeds/07_subrace.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('subrace').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('subrace').insert([
         {
           id: 1,
           race_id: 1,
